29840d082a2d937f3c60862e2363f01037796f1b
Harry262 <77656764+Harry262@users.noreply.github.com>
2023-04-15 05:42:28 -0700

    Add files via upload

---
+++ Assignment.py
@@ -0,0 +1,17 @@
+## QUESTION 1 ##
+ques1 = input("What is the capital of englad")
+ans1 = "london"
+## QUESTION 2 ##
+ques2 = input("When did pakistan won the worldcup")
+ans2 = input("1992")
+## IF ELSE STATEMENTS ##
+if ques1 == ans1:
+    print("That's the correct Answer")
+
+    if ques2 == ans2:
+        print("Great You Won")
+
+    else:
+        print("you lost")
+
+else ("oops thats not correct")
