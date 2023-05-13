70bfc5f65a74dc60ca35c96c0eae50e426c2860b
Harry262 <77656764+Harry262@users.noreply.github.com>
2023-05-13 17:56:08 +0500

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -1,18 +1,18 @@
 ## QUESTION 1 ##
-ques1 = input("What is the capital of englad")
-ans1 = "london"
+ques1 = input("What is the capital of England?")
+ans1 = "London"
+
 ## QUESTION 2 ##
-ques2 = input("When did pakistan won the worldcup")
-ans2 = input("1992")
+ques2 = input("When did Pakistan win the World Cup?")
+ans2 = "1992"
+
 ## IF ELSE STATEMENTS ##
-if ques1 == ans1:
-    print("That's the correct Answer")
+if ques1.lower() == ans1.lower():
+    print("That's the correct answer!")
 
     if ques2 == ans2:
-        print("Great You Won")
-
+        print("Great, you won!")
     else:
-        print("you lost")
-
+        print("Sorry, Pakistan did not win the World Cup in 1992.")
 else:
-    ("oops thats not correct")
+    print("Oops, that's not correct.")
